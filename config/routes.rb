3f3bae8b8e74f999134b173a482a1d6e10ba2fb2
福井洸希 <katuwakame@gmail.com>
2017-03-20 16:33:28 +0900

    マークアップするために最低限のルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'groups#index'
+  resources :groups, only: [:edit, :update] do
+    resources :messages, only: [:new, :create]
+  end
 end
