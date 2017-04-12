4ca953f6e5c5ef452b948c60a0f69fb7a83a6af2
福井洸希 <katuwakame@gmail.com>
2017-04-12 17:56:15 +0900

    add before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
    def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
       devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-
    end
-
-   def after_sign_out_path_for(resource)
-     '/users/sign_in' # サインアウト後のリダイレクト先URL
-   end
-
 end
