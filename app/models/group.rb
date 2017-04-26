12f9f6a9f25acd9e54b7d194c75c35828f2081ea
福井洸希 <katuwakame@gmail.com>
2017-04-26 13:45:52 +0900

    add association

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,7 @@ class Group < ApplicationRecord
   validates :name, presence: true
 
   has_many :user_groups
+  has_many :messages
   has_many :users, through: :user_groups
   accepts_nested_attributes_for :users
 end
