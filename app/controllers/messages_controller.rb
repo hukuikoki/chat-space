f2e327c74118678fe034dc6386d07d80c2cb4712
福井洸希 <katuwakame@gmail.com>
2017-04-26 14:35:47 +0900

    add create action and index instance

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,21 @@ class MessagesController < ApplicationController
     @group = Group.find(params[:group_id])
     @groups = current_user.groups
     @users = @group.users
+    @message = Message.new
+    @messages = @group.messages
+  end
+
+  def create
+    message = current_user.messages.new(create_params)
+    if message.save
+      redirect_to group_messages_path(message.group_id)
+    end
+  end
+
+  private
+
+  def create_params
+    params.require(:message).permit(:text).merge(group_id: params[:group_id])
   end
 end
 
