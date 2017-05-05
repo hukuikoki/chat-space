558d16615a873e6e78a3b1387382d49e99a5a87f
福井洸希 <katuwakame@gmail.com>
2017-05-05 20:49:30 +0900

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :name, presence: true, length: { maximum: 6 }
+
   has_many :user_groups
   has_many :messages
   has_many :groups, through: :user_groups
