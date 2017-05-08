c8297e5e81ee8b7aa08a6cf418084ede2dc31d34
福井洸希 <katuwakame@gmail.com>
2017-05-08 18:28:10 +0900

    add test code

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -13,9 +13,22 @@ describe Message do
       expect(message.errors[:text]).to include("を入力してください")
     end
 
-    it "is valid with a text" do
-      message = build(:message, text: "asdf")
-      expect(message).to be_valid
+    it "is valid with a same number user_id and user.id" do
+      user = build(:user)
+      message = build(:message, user_id: user.id)
+      expect(message.user_id).to eq user.id
+    end
+
+    it "is valid with a same number group_id and group.id" do
+      group = build(:group)
+      message = build(:message, group_id: group.id)
+      expect(message.group_id).to eq group.id
+    end
+
+    it "is invalid with a text that has more than 255 characters " do
+      message = build(:message, text: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+      message.valid?
+      expect(message.errors[:text][0]).to include("は255文字以内で入力してください")
     end
   end
 end
