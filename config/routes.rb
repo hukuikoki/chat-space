90986083e91addd8a188f078c7e38c0c1b688343
福井洸希 <katuwakame@gmail.com>
2017-05-26 15:58:34 +0900

    modified routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resources :users, only: :search
-    collection do
-      get 'search'
-    end
-
+  resources :users, only: :search do
+    get 'search', on: :collection
+  end
   resources :groups, only: [:index, :new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
