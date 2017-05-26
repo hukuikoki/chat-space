17de06307391de2508a422192bf21b6e640458d2
福井洸希 <katuwakame@gmail.com>
2017-05-26 15:47:40 +0900

    add search action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+
+  def search
+    @users = User.where('title LIKE(?)',"%#{search_params[:keyword]}%").order('title ASC').limit(20)
+
+    respond_to do |format|
+      format.json { render json: @users }
+    end
+  end
+
 end
