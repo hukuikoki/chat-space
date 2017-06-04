d43f840e22e56ac5b1d9ba87289468be8e5935ab
福井洸希 <katuwakame@gmail.com>
2017-06-04 11:58:01 +0900

    modified

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -8,7 +8,7 @@ $(document).on('turbolinks:load', function(){
       return html;
     };
 
-    function RemoveUserHtml(id, name) {
+    function RemoveUserHTML(id, name) {
       var html = `<li class= "remove-group-user">
                   <input type= 'hidden' name= 'group[user_ids][]' value= " ${ id } ">
                   <p class= 'chat-group-user__name'> ${ name } </p>
@@ -18,33 +18,33 @@ $(document).on('turbolinks:load', function(){
     };
 
      $('#user-search-field').on('keyup', function(e) {
-      input = $.trim($('#user-search-field').val());
-      $('.add-group-user').remove();
-      if (input.length !== 0) {
-        $.ajax({
-          type: 'GET',
-          url: '/users/search',
-          data: { keyword: input },
-          dataType: 'json',
-        })
-        .done(function(data) {
-          $.each(data, function(i, data) {
-            html = buildHTML(data);
-            $('#user-search-result').append(html);
-          });
-        })
-        .fail(function(json){
-          alert('グループを作成してください。');
-        });
-      };
+       input = $.trim($('#user-search-field').val());
+       $('.add-group-user').remove();
+       if (input.length !== 0) {
+         $.ajax({
+           type: 'GET',
+           url: '/users/search',
+           data: { keyword: input },
+           dataType: 'json',
+         })
+         .done(function(data) {
+           $.each(data, function(i, data) {
+             html = buildHTML(data);
+             $('#user-search-result').append(html);
+           });
+         })
+         .fail(function(json){
+           alert('グループを作成してください。');
+         });
+       };
      });
 
       $(document).on('click', '#add_button', function() {
-        var id = $(this).data('user-id');
-        var name = $(this).data('user-name');
         var user = $(this).parent();
         $(user).remove();
-        var new_html = RemoveUserHtml(id, name);
+        var id = $(this).data('user-id');
+        var name = $(this).data('user-name');
+        var new_html = RemoveUserHTML(id, name);
         $(' #chat-group-users').append(new_html);
       });
 
