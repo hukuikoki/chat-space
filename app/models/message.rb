29f3ab4e40dd3f99a08b86850f00b318a71b7b30
福井洸希 <katuwakame@gmail.com>
2017-06-12 17:11:37 +0900

    add setting code at message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,8 @@ class Message < ApplicationRecord
   validates :user_id, presence: true
   validates :group_id, presence: true
 
+  mount_uploader :image, ImageUploader
+
   belongs_to :user
   belongs_to :group
 end
