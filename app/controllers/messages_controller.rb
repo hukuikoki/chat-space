5a34a36c27c0fce7e630d55024b89db7203783ea
福井洸希 <katuwakame@gmail.com>
2017-07-27 23:07:22 +0900

    add format in messages_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,21 +1,26 @@
 class MessagesController < ApplicationController
   def index
     @group = Group.find(params[:group_id])
-    @groups = current_user.groups
-    @users = @group.users
+    @groups = current_user.groups.includes(:messages)
+    @users = @group.users.includes(:messages)
     @message = Message.new
-    @messages = @group.messages
+    @messages = @group.messages.includes(:user)
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def create
     @message = current_user.messages.new(create_params)
-    if @message.save
-       respond_to do |format|
-         format.json { render 'messages', handlers: 'jbuilder' }
-       end
-    else
-      redirect_to  group_messages_url(@message.group_id), alert: 'メッセージが送信されませんでした。'
-    end
+      if @message.save
+        respond_to do |format|
+          format.html { group_messages_url(@message.group_id) }
+          format.json { render 'create', handlers: 'jbuilder' }
+        end
+      else
+        redirect_to  group_messages_url(@message.group_id), alert: 'メッセージが送信されませんでした。'
+      end
   end
 
   private
