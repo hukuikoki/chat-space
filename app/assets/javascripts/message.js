c5f22802f6e4d3718baafd0960b9c3f5880f1029
福井洸希 <katuwakame@gmail.com>
2017-07-27 23:06:26 +0900

    add automatic-updating code

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -11,7 +11,7 @@ $(document).on('turbolinks:load', function() {
 
     var html = `<ul class="chat__main__body">
                   <li class="chat__main__body__message">
-                    <div class="chat__main__body__message__header">
+                    <div class="chat__main__body__message__header" data-message-id="${message.id}">
                       <p class="chat__main__body__message__header--name">${ message.name }</p>
                       <p class="chat__main__body__message__header--then">${ message.created_at }</p>
                     </div>
@@ -48,6 +48,31 @@ $(document).on('turbolinks:load', function() {
         $('input').prop('disabled', false);
       });
     });
-  });
 
+      //自動更新の関数
+      var interval = setInterval(function() {
+        if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+      $.ajax({
+        type: 'GET',
+        url: location.href,
+        dataType: 'json'
+      })
+      .done(function(json) {
+        var insertHTML = '';
+        json.messages.forEach(function(message) {
+            insertHTML += buildHTML(message);
+        });
+        var $messages = $('.chat__main');
+        $messages.empty();
+        $messages.append(insertHTML);
+      })
+      .fail(function(json) {
+        alert('自動更新に失敗しました');
+      });
+      } else {
+      clearInterval(interval);
+     }} , 5 * 1000 );
+  });
 });
+
+
